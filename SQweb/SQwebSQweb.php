80010e2af96134a87327fc39ac9bd0b9469e491d
Pierre Lavaux <pierre.lvx@gmail.com>
2016-10-12 18:09:02 +0200

    PHP coding style
    Signed-off-by: Pierre Lavaux <pierre.lvx@gmail.com>

--- SQweb/SQwebSQweb.php
+++ SQweb/SQwebSQweb.php
@@ -6,30 +6,30 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SQwebSQweb
 {
-	private $config = [];
-	public $abo = 0;
-	public $script = null;
-	public $button = null;
+    private $config = [];
+    public $abo = 0;
+    public $script = null;
+    public $button = null;
 
-	public function __construct(ContainerInterface $container)
-	{
-		$this->config['id_site'] = $container->getParameter('id_site');
-		$this->config['debug'] = $container->getParameter('debug') ?: 'false';
-		$this->config['targeting'] = $container->getParameter('targeting') ?: 'false';
-		$this->config['beacon'] = $container->getParameter('beacon') ?: 'false';
-		$this->config['dwide'] = $container->getParameter('dwide') ?: 'false';
-		$this->config['lang'] = $container->getParameter('lang');
-		$this->config['message'] = $container->getParameter('message');
-		$this->script();
-		$this->checkCredits();
-		$this->button();
-	}
+    public function __construct(ContainerInterface $container)
+    {
+        $this->config['id_site'] = $container->getParameter('id_site');
+        $this->config['debug'] = $container->getParameter('debug') ?: 'false';
+        $this->config['targeting'] = $container->getParameter('targeting') ?: 'false';
+        $this->config['beacon'] = $container->getParameter('beacon') ?: 'false';
+        $this->config['dwide'] = $container->getParameter('dwide') ?: 'false';
+        $this->config['lang'] = $container->getParameter('lang');
+        $this->config['message'] = $container->getParameter('message');
+        $this->script();
+        $this->checkCredits();
+        $this->button();
+    }
 
-	/**
-	 * Ajoute le script SQweb
-	 */
-	private function script()
-	{
+    /**
+     * Ajoute le script SQweb
+     */
+    private function script()
+    {
         $this->script = '
 <script>
 	/* SDK SQweb Symfony 1.0 */
@@ -46,16 +46,16 @@ class SQwebSQweb
 	script.src = "//cdn.sqweb.com/sqweb-beta.js";
 	document.getElementsByTagName("head")[0].appendChild(script);
 </script>';
-	}
+    }
 
-	private function button()
-	{
-		$this->button = '<div class="sqweb-button-mp"><div class="sqw-btn-mp"></div></div>';
-	}
+    private function button()
+    {
+        $this->button = '<div class="sqweb-button-mp"><div class="sqw-btn-mp"></div></div>';
+    }
 
-	private function checkCredits()
-	{
-		$response = null;
+    private function checkCredits()
+    {
+        $response = null;
         if (isset($_COOKIE['sqw_z']) && null !== $this->config['id_site']) {
             $curl = curl_init();
             curl_setopt_array($curl, [
@@ -76,11 +76,11 @@ class SQwebSQweb
         if ($response !== null && $response->status === true && $response->credit > 0) {
             $this->abo = $response->credit;
         } else {
-        	$this->abo = 0;
+            $this->abo = 0;
         }
-	}
+    }
 
-	public function sqwBalise($balise, $match)
+    public function sqwBalise($balise, $match)
     {
         if (preg_match('/<(\w+)(?(?!.+\/>).*>|$)/', $match, $tmp)) {
             if (!isset($balise)) {
@@ -172,7 +172,7 @@ class SQwebSQweb
         }
         $date = date_create($date);
         $now = date_create('now');
-       	date_modify($now, '-'.$wait.' days');
+        date_modify($now, '-'.$wait.' days');
         return $date < $now;
     }
 }
